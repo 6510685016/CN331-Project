445c9bc9f4c1086341726ff604b5c876db605688
6510615252 <6510615252@student.tu.ac.th>
2024-12-01 17:44:52 +0700

    functional progression node

--- catchcash/mainapp/views.py
+++ catchcash/mainapp/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.db import models
 from django.contrib import messages
 from django.forms import ValidationError
 from django.http import HttpResponse
@@ -401,12 +402,30 @@ def wallet_detail(request, id):
 
 def progression(request):
     account = request.user.account  # ดึง Account ของผู้ใช้ปัจจุบัน
+    wallets = account.wallets.all()  # ดึงทุก wallet ที่ผู้ใช้งานมี
+    
+    # ดึงข้อมูลจำนวน Wallet, Statement, Preset สำหรับทุก wallet
+    wallet_count = wallets.count()
+    preset_count = sum(wallet.presets.count() for wallet in wallets)
+    mission_count = sum(wallet.missions.count() for wallet in wallets)
+    scope_count = sum(wallet.scopes.count() for wallet in wallets)
+    mission_count = sum(wallet.missions.count() for wallet in wallets)
 
-    # ดึงจำนวน Wallet ที่ผู้ใช้งานมี
-    wallet_count = account.wallets.count()
-    return render(request, 'progression.html',{
+    total_income = sum(wallet.statements.filter(type='in').aggregate(total_income=models.Sum('amount'))['total_income'] or 0 for wallet in wallets)
+
+    has_successful_mission = any(
+        mission.is_successful() for wallet in wallets for mission in wallet.missions.all()
+    )
+
+    return render(request, 'progression.html', {
         'wallet_count': wallet_count,
+        'preset_count': preset_count,
+        'mission_count': mission_count,
+        'scope_count': scope_count,
+        'total_income': total_income,
+        'has_successful_mission': has_successful_mission,
     })
+
 
 
 def trophy(request):
